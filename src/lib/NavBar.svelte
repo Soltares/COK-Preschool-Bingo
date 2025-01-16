8273a8226cb3a89661e58942015b545022d73185
Christopher Davis <Christopher.Davis.013@gmail.com>
2025-01-16 09:56:21 -0500

    Volunteer NavBar link

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	let navLinks = [
-		{ href: '/#', label: 'HOME' },
+		{ href: '/#', label: 'HOME', className: 'hidden sm:block' },
 		{ href: '/#about', label: 'ABOUT' },
 		{ href: '/#buy', label: 'TICKETS' },
 		{ href: '/#food', label: 'FOOD' },
-		{ href: '/#where', label: 'WHERE' }
+		{ href: '/#where', label: 'WHERE' },
+		{ href: '/volunteer#help', label: 'VOLUNTEER' }
 	]
 </script>
 
 <nav class="fixed top-0 z-20 flex h-14 w-full place-content-evenly items-center bg-black text-center">
-	{#each navLinks as { href, label }}
-		<a class="font-poppins block grow px-2 py-4 text-white transition-all hover:bg-[#ccc] hover:text-black" {href}>{label}</a>
+	{#each navLinks as { href, label, className }}
+		<a class="font-poppins block grow px-2 py-4 text-white transition-all hover:bg-[#ccc] hover:text-black {className}" {href}>{label}</a>
 	{/each}
 </nav>
