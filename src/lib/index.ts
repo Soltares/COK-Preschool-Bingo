068d403f4f7783841b6bddff02ce6bad6249a7b8
Christopher Davis <Christopher.Davis.013@gmail.com>
2025-01-27 13:48:54 -0500

    Added ShopRite

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -9,7 +9,8 @@ export let sponsors = [
 	{ name: 'Mr. V Laser Design', logoUrl: '/mr-v-logo.png', linkUrl: 'https://mrvlaserdesign.com/', class: '!rounded-none' },
 	{ name: 'Nancy Hines', logoUrl: '/nancyh.png' },
 	{ name: 'Chick-fil-A', logoUrl: '/chick-fil-a-logo.jpg', linkUrl: 'https://www.chick-fil-a.com/locations/md/bel-air' },
-	{ name: 'Aberdeen IronBirds', logoUrl: '/Aberdeen_IronBirds_logo.svg', linkUrl: 'https://www.milb.com/aberdeen', class: '!rounded-none' }
+	{ name: 'Aberdeen IronBirds', logoUrl: '/Aberdeen_IronBirds_logo.svg', linkUrl: 'https://www.milb.com/aberdeen', class: '!rounded-none' },
+	{ name: "Klein's ShopRite", logoUrl: '/ShopRite_(United_States)_logo.svg', linkUrl: 'https://kleinsfm.com/locations/shoprite-at-the-festival-at-bel-air/', class: '!rounded-none' }
 ]
 
 export let trucks = [
